0414512c5ca14b23b14ce1c0b0b3e5641695411b
MuzCodeSmith <shaikhmuzffar82@gmail.com>
2025-09-25 17:09:54 +0530

    created server, added route based response

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-let fruit ="apple";
+const http = require('http');
+
+http.createServer((req,res)=>{
+    
+    let response = '';
+
+    if(req.url == '/'){
+        response = "Home Page"
+    }
+    if(req.url == '/about'){
+        response = "About Page"
+    }
+    res.setHeader('Content-Type','text/html');
+    res.write(response);
+    res.end();
+
+}).listen(8000);
+
